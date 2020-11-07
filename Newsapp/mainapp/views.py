9a15e364a9c97078867c3c5452b8d7fe1874f511
ec18213 <s.akhtar@se18.qmul.ac.uk>
2020-11-07 16:47:44 +0000

    Users Can register

--- Newsapp/mainapp/views.py
+++ Newsapp/mainapp/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, Http404
 #for testing remove later
 from django.views.decorators.csrf import csrf_exempt
 
+from .models import Member, Article
+
 # Create your views here.
 
 appname = 'News' #Application name
@@ -18,13 +20,25 @@ def register(request):
 
 @csrf_exempt # just for testing purposes cant pass csrf using insomnia
 def makeAccount(request):
-    email = request.POST["Email"]
-    name = request.POST["Flname"]
-    dob = request.POST["DOB"]
-    password = request.POST["Password"]
+    #taking all form values out of the POST Dictionairy
+    #USE INSOMNIA TO TEST HAVENT ACTUALLY SET THE FROM ACTION TO THIS YET
 
-    if email and name and dob and password:
-        HttpResponse("bad man")
+    if 'Username' in request.POST and 'Email' in request.POST and 'Flname' in request.POST and 'DOB' in request.POST and 'Password' in request.POST:
+        username = request.POST["Username"]
+        email = request.POST["Email"]
+        name = request.POST["Flname"]
+        dob = request.POST["DOB"]
+        password = request.POST["Password"]
+
+        #return HttpResponse("lol")
+        #Save the member
+        member = Member(username = username, email = email, first_name = name, date_of_birth = dob)
+        member.set_password(password) #Automatically Hashes the password
+
+        try: member.save()
+        except IntegrityError: raise Http404('Error User could not be registered') # user is registered but integrity error not working
+        # Potentially add feature to email new member 
+        context = {'appname': appname, 'userName': name}
+        return render(request, 'mainapp/userRegistered.html', context)
     else:
-        raise Http404("Wasteman")
-#not complete will finish this
+        raise Http404("RAHH U MAD")
