10f0b788fc7b4928324f84b875b446e8202ce329
ec18213 <s.akhtar@se18.qmul.ac.uk>
2020-11-07 14:34:26 +0000

    Added models for articles and members

--- Newsapp/mainapp/admin.py
+++ Newsapp/mainapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Member, Article
+
+#admin login details username: admin, password: password
 # Register your models here.
+
+admin.site.register(Member)
+admin.site.register(Article)
--- Newsapp/mainapp/models.py
+++ Newsapp/mainapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User # import premade user model
 
 # Create your models here.
+
+class Member(User): # Extending the django User model, as it already includes the fields email, name, password
+    date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
+    interest_tag = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+class Article(models.Model):
+    article_name = models.CharField(max_length=200)
+    article_author = models.CharField(max_length=50)
+    article_date = models.DateField(auto_now=False, auto_now_add=False)
+    article_tag = models.CharField(max_length=50)
+    article_contents = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return self.article_name
+
+
