b0b10ac2b237f1b01ea3dad4819a04516ac00d76
ec18213 <s.akhtar@se18.qmul.ac.uk>
2020-11-09 13:52:15 +0000

    Login is Now Working

--- Newsapp/mainapp/urls.py
+++ Newsapp/mainapp/urls.py
@@ -4,6 +4,8 @@ from mainapp import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
-    path('makeAccount', views.makeAccount, name='makeaccount' ),
+    path('makeAccount', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('signin', views.signin, name='signin')
 ]
 
--- Newsapp/mainapp/views.py
+++ Newsapp/mainapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 #for testing remove later
 from django.views.decorators.csrf import csrf_exempt
-
+import datetime
 from .models import Member, Article
 
 # Create your views here.
@@ -18,8 +18,12 @@ def register(request):
     context = {'appname': appname}
     return render(request, 'mainapp/register.html', context)
 
+def login(request):
+    context = {'appname': appname}
+    return render(request, 'mainapp/login.html', context)
+
 @csrf_exempt # just for testing purposes cant pass csrf using insomnia
-def makeAccount(request):
+def signup(request):
     #taking all form values out of the POST Dictionairy
     #USE INSOMNIA TO TEST HAVENT ACTUALLY SET THE FROM ACTION TO THIS YET
 
@@ -42,3 +46,38 @@ def makeAccount(request):
         return render(request, 'mainapp/userRegistered.html', context)
     else:
         raise Http404("RAHH U MAD")
+
+@csrf_exempt
+def signin(request):
+    if 'Username' in request.POST and 'Password' in request.POST:
+        username = request.POST['Username']
+        password = request.POST['Password']
+
+        try: member = Member.objects.get(username = username) # Checking the given username exists
+        except Member.DoesNotExist : raise Http404("Member does not exist")
+
+        if member.check_password(password): #Compare the password given to the hashed password in the DB
+            #Set session variables for the user logged in
+            request.session['Username'] = username
+            request.session['Password'] = password 
+
+            #Set cookie
+            timeNow = datetime.datetime.utcnow()
+            age = 365 * 24 * 60 * 60
+            delta = timeNow + datetime.timedelta(seconds=age)
+            format = "%a, %d-%b-%Y %H:%M:%S GMT"
+            expiration = datetime.datetime.strftime(delta, format)
+            #Set login in context to be passed to template
+            context = {'appname': appname, 'logIn': True, 'name': username}
+            #create variable res to store render so cookie can be set to it
+            res = render(request, 'mainapp/login.html', context) # change from login
+            res.set_cookie('lastLogin', timeNow, expires=expiration)
+            return res
+        else:
+            raise Http404('Incorrect Password')
+
+
+        return HttpResponse("Nice it works")
+    else:
+        context = {'appname': appname, 'error': 'Please Enter the correct login in details'}
+        return render(request, 'mainapp/login.html', context)
